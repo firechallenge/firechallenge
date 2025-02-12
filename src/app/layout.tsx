350833721894b2daf275badbf887a48afa221e65
Chase Willden <chase.willden@gmail.com>
2025-02-12 16:17:57 -0700

    init landing page

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Image from "next/image";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fire Challenge",
+  description: "Challenges to learn anything",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="min-h-screen">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased p-3 min-h-screen`}
+      >
+        <header className="flex justify-between bg-zinc-800 py-2 px-4 rounded-xl">
+          <div className="flex flex-row items-center gap-4">
+            <Image src="/logo4.svg" alt="Logo" width={30} height={45} />
+            <h1 className="text-2xl font-bold">Fire Challenge</h1>
+          </div>
+          <nav></nav>
+        </header>
+        <main className="min-h-screen">{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+export default function Home() {
+  return (
+    <div>
+      <section className="flex flex-col items-center justify-center">
+        <h1 className="mx-6 mt-12 w-[300px] bg-gradient-to-b from-black/80 to-black bg-clip-text pb-4  text-center text-5xl font-extrabold leading-tight text-transparent md:!w-full lg:!mt-20 lg:text-6xl xl:leading-snug dark:from-white dark:to-[#AAAAAA]">
+          Fuel the Learning
+        </h1>
+        <p className="mx-6 max-h-[112px] w-[315px] text-center font-mono text-xl text-[#666666] md:max-h-[96px] md:w-[700px] md:text-xl dark:text-[#888888]">
+          Fire Challenge is a challenge-based system to allow for you to have
+          control over your learning.
+        </p>
+      </section>
+      <section className="flex flex-row justify-center gap-8 items-center mt-10">
+        <div className="w-[350px] h-[300px] bg-neutral-800 flex flex-col items-center justify-center rounded-xl border border-neutral-700 cursor-pointer transition-all duration-200 hover:border-blue-500">
+          <h3 className="text-center text-3xl font-bold font-mono">Python</h3>
+          <p className="w-[250px] text-center tracking-wide text-neutral-400 mt-4">
+            The Python Programming Language
+          </p>
+        </div>
+        <div className="w-[350px] h-[300px] bg-neutral-800 flex flex-col items-center justify-center rounded-xl border border-neutral-700 cursor-pointer transition-all duration-200 hover:border-blue-500">
+          <h3 className="text-center text-3xl font-bold font-mono">Web Tech</h3>
+          <p className="w-[250px] text-center tracking-wide text-neutral-400 mt-4">
+            Web technologies including HTML, JS, CSS, TS, etc.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
